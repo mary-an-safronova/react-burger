25955ff85c044d65162f1c5063cb561d2c78f9d7
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-04-26 19:06:48 +0300

    feat: Настроено расширение Redux DevTools

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { legacy_createStore as createStore} from 'redux'
+import { compose, legacy_createStore as createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/app/app';
+import reportWebVitals from './reportWebVitals';
 import { rootReducer } from './services/reducers/index';
 
-const store = createStore(rootReducer);
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancer);
 
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root')  as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -19,3 +32,5 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+reportWebVitals();
