71a86245971d88ead40f2351b0b7bccdd725d12d
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-05-22 12:40:06 +0300

    feat: описан POST-запрос на экране /forgot-password

--- src/pages/forgot-password/forgot-password.jsx
+++ src/pages/forgot-password/forgot-password.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import forgotPasswordStyle from './forgot-password.module.css';
 import { EmailInput, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import { request } from '../../utils/api';
 
 const ForgotPassword = () => {
 
     const [email, setEmail] = useState('');
+    const navigate = useNavigate();
+
+    const postMail = (evt) => {
+        evt.preventDefault();
+
+        request('/password-reset', 'POST', JSON.stringify({ email: email }))
+        .then((success, data) => {
+            if (success) {
+                setEmail(data);
+                navigate('/reset-password', { replace: true });
+            }
+        })
+        .catch((err) => { console.log(err) });
+    }
 
     return (
         <div className={forgotPasswordStyle.wrapper}>
-            <form className={forgotPasswordStyle.form}>
+            <form className={forgotPasswordStyle.form} onSubmit={postMail}>
                 <fieldset className={forgotPasswordStyle.wrap}>
                     <legend className={`${forgotPasswordStyle.title} text text_type_main-medium mb-6`}>Восстановление пароля</legend>
                     <EmailInput
@@ -21,9 +36,7 @@ const ForgotPassword = () => {
                         isIcon={false}
                         extraClass="mb-6"
                     />
-                    <Link to={'/reset-password'}>
-                        <Button htmlType="submit" type="primary" size="medium" disabled={email ? false : true}>Восстановить</Button>
-                    </Link>
+                    <Button htmlType="submit" type="primary" size="medium" disabled={email ? false : true}>Восстановить</Button>
                     <p className="text text_type_main-default text_color_inactive mt-20 mb-4">Вспомнили пароль?
                         <Link className={`${forgotPasswordStyle.link} text text_type_main-small ml-2`} to={'/login'}>Войти</Link>
                     </p>
