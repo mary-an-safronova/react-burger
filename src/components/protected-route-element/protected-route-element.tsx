6c63b68a833b2e6ec234f5679f82301d3e91a683
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-07-06 15:34:23 +0300

    fix: исправлен тип element в файле ProtectedRouteElement

--- src/components/protected-route-element/protected-route-element.tsx
+++ src/components/protected-route-element/protected-route-element.tsx
@@ -1,17 +1,16 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "../../services/hooks";
 import { PATH } from "../../utils/api";
-import { ReactNode } from "react"
 
 interface ProtectedRouteElementProps {
-    element: ReactNode;
+    element: JSX.Element;
 }
 
 const ProtectedRouteElement = ({ element }: ProtectedRouteElementProps) => {
     const location = useLocation();
     const auth = useSelector((state) => state.auth.auth);
 
-    return auth ? element : <Navigate to={PATH.LOGIN} state={{ from: location }} />;
+    return( auth ? element : <Navigate to={PATH.LOGIN} state={{ from: location }} />)
 }
 
 export default ProtectedRouteElement;
